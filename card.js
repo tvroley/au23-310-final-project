daed8852566e017f5ebd32179ab5a5b320e310c4
Theodore Roley <tvroley@uw.edu>
2023-12-01 12:25:13 -0800

    graded card class

--- card.js
+++ card.js
@@ -11,3 +11,17 @@ class Card {
         return `${this.year} ${this.brand} ${this.cardSet} #${this.cardNumber} ${this.player}`;
     }
 }
+
+class GradedCard extends Card {
+    constructor(year, brand, cardSet, cardNumber, player, gradingCompany, certificationNumber, gradedLabelTitle, gradedCardImageLink) {
+        super(year, brand, cardSet, cardNumber, player);
+        this.gradingCompany = gradingCompany;
+        this.certificationNumber = certificationNumber;
+        this.gradedLabelTitle = gradedLabelTitle;
+        this.gradedCardImageLink = gradedCardImageLink;
+    }
+
+    toString() {
+        return super.toString() + ` ${this.gradedLabelTitle} certification number: ${this.certificationNumber}`;
+    }
+}
